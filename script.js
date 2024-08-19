77ebc600c9d6939cc94c6ee0df175d3a3f4ec78a
wilson ikeda <wilsonikedakoffi7@gmail.com>
2024-08-19 17:39:31 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+/**
+ * Ce que vous visez
+
+Dans ce point de contrôle, votre tâche consistera à collecter 300 points en résolvant des problèmes dans CodeWars.
+
+
+Instructions
+
+150 points seront gagnés à partir de problèmes présélectionnés, couvrant les bases (boucle, conditionnel, types de données, etc.)
+150 points seront gagnés de manière flexible, ce qui vous permettra de choisir librement les problèmes à résoudre (différentes difficultés récompensent différents points (2, 8, 32 et 128 pour les niveaux les plus difficiles).
+Les problèmes présélectionnés sont tous de niveau débutant (offrant ainsi 2 points par solution).
+
+PS : Gardez à l’esprit que certains points sont automatiquement attribués à l’utilisateur lors du classement (et d’autres activités pas forcément liées à la résolution de problèmes. Une explication complète du fonctionnement du système de points peut être trouvée ici).
+
+Vous trouverez une sélection de problèmes dans cette liste
+ */
